80e9c885d04593245a2034979570a19ec2431af9
Timm Felden <feldentm@informatik.uni-stuttgart.de>
2015-02-10 18:23:51 +0100

    added some test restrictions

---
+++ src/de/ust/skill/common/java/restrictions/NonNull.java
@@ -0,0 +1,26 @@
+package de.ust.skill.common.java.restrictions;
+
+import de.ust.skill.common.java.api.SkillException;
+import de.ust.skill.common.java.internal.SkillObject;
+
+/**
+ * A nonnull restricition. It will ensure that field data is non null.
+ * 
+ * @author Timm Felden
+ */
+public class NonNull<T extends SkillObject> implements FieldRestriction<T> {
+    private static final NonNull<?> instance = new NonNull<>();
+
+    private NonNull() {
+    }
+
+    public static NonNull<?> get() {
+        return instance;
+    }
+
+    @Override
+    public void check(SkillObject value) throws SkillException {
+        if (value == null)
+            throw new SkillException("Null value violates @NonNull.");
+    }
+}
---
+++ src/de/ust/skill/common/java/restrictions/TypeRestriction.java
@@ -0,0 +1,10 @@
+package de.ust.skill.common.java.restrictions;
+
+/**
+ * An abstract type restriction that can be asked to check a whole type.
+ *
+ * @author Timm Felden
+ */
+public interface TypeRestriction {
+    // does nothing in particular for now
+}
