035aa9fb7fdc4b008fd72a69d0dce8b9a66a6a3a
Timm Felden <feldentm@informatik.uni-stuttgart.de>
2016-07-04 17:09:15 +0200

    made skill object serializable for comparison against java

--- src/de/ust/skill/common/java/internal/SkillObject.java
+++ src/de/ust/skill/common/java/internal/SkillObject.java
@@ -1,108 +1,135 @@
 package de.ust.skill.common.java.internal;
 
+import java.io.Serializable;
+
 /**
- * The root of the hierarchy of instances of skill user types. Annotations can store arbitrary objects, thus this type
- * has to exist explicitly.
+ * The root of the hierarchy of instances of skill user types. Annotations can
+ * store arbitrary objects, thus this type has to exist explicitly.
  * 
  * @author Timm Felden
- * @note This type definition is in internal, because we have to protect setSkillID from the user
+ * @note This type definition is in internal, because we have to protect
+ *       setSkillID from the user
  */
 // TODO create a builder for skill objects
-public abstract class SkillObject {
+public abstract class SkillObject implements Serializable {
 
-    /**
-     * The constructor is protected to ensure that users do not break states accidentally
-     */
-    protected SkillObject(long skillID) {
-        this.skillID = skillID;
-    }
+	/**
+	 * @note Java Serialization is only usable, if specification is both,
+	 *       complete and without any distributed/onDemand fields. Both cases
+	 *       would cause unknown decomposed field information to float around
+	 *       without any means of the Java Serializer to know how to recompose
+	 *       them correctly. For change-tolerant serialization use SKilL. This
+	 *       mechanism has been implemented for the purpose of comparison only.
+	 */
+	private static final long serialVersionUID = 0xDecaFade00005c11L;
 
-    /**
-     * @return the skill name of this type
-     */
-    public abstract String skillName();
+	/**
+	 * The constructor is protected to ensure that users do not break states
+	 * accidentally
+	 */
+	protected SkillObject(long skillID) {
+		this.skillID = skillID;
 
-    /**
-     * -1 for new objects<br>
-     * 0 for deleted objects<br>
-     * everything else is the ID of an object inside of a file
-     */
-    protected long skillID;
+		new Thread();
+	}
 
-    /**
-     * @return whether the object has been deleted
-     */
-    public boolean isDeleted() {
-        return 0 == skillID;
-    }
+	/**
+	 * @return the skill name of this type
+	 */
+	public abstract String skillName();
 
-    /**
-     * Do not rely on skill ID if you do not know exactly what you are doing.
-     */
-    public final long getSkillID() {
-        return skillID;
-    }
+	/**
+	 * -1 for new objects<br>
+	 * 0 for deleted objects<br>
+	 * everything else is the ID of an object inside of a file
+	 */
+	transient protected long skillID;
 
-    final void setSkillID(long skillID) {
-        this.skillID = skillID;
-    }
+	/**
+	 * @return whether the object has been deleted
+	 */
+	public boolean isDeleted() {
+		return 0 == skillID;
+	}
 
-    /**
-     * reflective setter
-     *
-     * @param field
-     *            a field declaration instance as obtained from the storage pools iterator
-     * @param value
-     *            the new value of the field
-     * @note if field is not a distributed field of this type, then anything may happen
-     */
-    public <T> void set(de.ust.skill.common.java.api.FieldDeclaration<T> field, T value) {
-        field.setR(this, value);
-    }
+	/**
+	 * Do not rely on skill ID if you do not know exactly what you are doing.
+	 */
+	public final long getSkillID() {
+		return skillID;
+	}
 
-    /**
-     * reflective getter
-     *
-     * @param field
-     *            a field declaration instance as obtained from the storage pools iterator
-     * @note if field is not a distributed field of this type, then anything may happen
-     * @note the second type parameter of field has to be this.type. Unfortunately Java wont let us override the type
-     *       parameter on each overload, although this pattern would automagically make everything work as intended and
-     *       the user would always know whether using a field declaration on a specific instance would work well.
-     */
-    public <T> T get(de.ust.skill.common.java.api.FieldDeclaration<T> field) {
-        return field.getR(this);
-    }
+	final void setSkillID(long skillID) {
+		this.skillID = skillID;
+	}
 
-    /**
-     * potentially expensive but more pretty representation of this instance.
-     */
-    public String prettyString() {
-        StringBuilder sb = new StringBuilder("SkillObject(this: ").append(this);
-        return sb.append(")").toString();
-    }
+	/**
+	 * reflective setter
+	 *
+	 * @param field
+	 *            a field declaration instance as obtained from the storage
+	 *            pools iterator
+	 * @param value
+	 *            the new value of the field
+	 * @note if field is not a distributed field of this type, then anything may
+	 *       happen
+	 */
+	public <T> void set(de.ust.skill.common.java.api.FieldDeclaration<T> field, T value) {
+		field.setR(this, value);
+	}
 
-    public static final class SubType extends SkillObject implements NamedType {
-        private final StoragePool<?, ?> τPool;
+	/**
+	 * reflective getter
+	 *
+	 * @param field
+	 *            a field declaration instance as obtained from the storage
+	 *            pools iterator
+	 * @note if field is not a distributed field of this type, then anything may
+	 *       happen
+	 * @note the second type parameter of field has to be this.type.
+	 *       Unfortunately Java wont let us override the type parameter on each
+	 *       overload, although this pattern would automagically make everything
+	 *       work as intended and the user would always know whether using a
+	 *       field declaration on a specific instance would work well.
+	 */
+	public <T> T get(de.ust.skill.common.java.api.FieldDeclaration<T> field) {
+		return field.getR(this);
+	}
 
-        SubType(StoragePool<?, ?> τPool, long skillID) {
-            super(skillID);
-            this.τPool = τPool;
-        }
+	/**
+	 * potentially expensive but more pretty representation of this instance.
+	 */
+	public String prettyString() {
+		StringBuilder sb = new StringBuilder("SkillObject(this: ").append(this);
+		return sb.append(")").toString();
+	}
 
-        @Override
-        public StoragePool<?, ?> τPool() {
-            return τPool;
-        }
+	public static final class SubType extends SkillObject implements NamedType {
+		/**
+		 * should not happen
+		 */
+		private static final long serialVersionUID = 3283783094243102233L;
+		
+		transient private final StoragePool<?, ?> τPool;
 
-        @Override
-        public String toString() {
-            return skillName() + "#" + skillID;
-        }
+		SubType(StoragePool<?, ?> τPool, long skillID) {
+			super(skillID);
+			this.τPool = τPool;
+		}
 
-        @Override
-        public String skillName() {
-            return τPool.name;
-        }
-    }
+		@Override
+		public StoragePool<?, ?> τPool() {
+			return τPool;
+		}
+
+		@Override
+		public String toString() {
+			return skillName() + "#" + skillID;
+		}
+
+		@Override
+		public String skillName() {
+			return τPool.name;
+		}
+	}
 }
